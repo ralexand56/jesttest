0e6bf2765361fd94f4c63de31f6e5c26b30d8e34
ralexand56 <ralexand56@live.com>
2018-12-20 16:53:01 -0800

    added regular test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,7 +6,13 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 
-  expect(div.innerHTML).toContain('Hi there');
+  expect(div.innerHTML).toContain('Hi There');
   
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('adds numbers', () => {
+  const add = (a: number, b: number) => a + b;
+
+  expect(add(2, 3)).toBe(5);
+})
