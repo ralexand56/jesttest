df70923a02896aa006341f1da1c26c7c0ae5bf18
ralexand56 <ralexand56@live.com>
2018-12-20 19:04:53 -0800

    reduce test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,12 +7,39 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
 
   expect(div.innerHTML).toContain('Hi There');
-  
+
   ReactDOM.unmountComponentAtNode(div);
 });
 
 it('adds numbers', () => {
   const add = (a: number, b: number) => a + b;
 
-  expect(add(2, 3)).toBe(5);
-})
+  expect(add(6, -1)).toBe(5);
+});
+
+it('palindrome', () => {
+  const isPalindrome = (word: string) =>
+    word
+      .split('')
+      .reverse()
+      .join('');
+
+  expect(isPalindrome('anna')).toBe('anna');
+});
+
+it('getSubsequence', () => {
+  const seq = (s1: string, s2: string) => {
+    return s1
+      .split('')
+      .reduce(
+        (acc: Record<string, number>[], curr: string, ind: number) =>
+          s2.split('').indexOf(curr, ind) > -1
+            ? [...acc, { [curr]: s2.split('').indexOf(curr, ind) }]
+            : [...acc],
+        [],
+      )
+      .map(x => Object.keys(x));
+  };
+
+  expect(seq('ABAZDC', 'BACBAD')).toBe([]);
+});
